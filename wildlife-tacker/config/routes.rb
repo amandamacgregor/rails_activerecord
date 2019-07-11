35937b5566d846f1a8f544c667655bf71fc52ff8
Ubuntu <ubuntu@ip-172-31-34-124.us-east-2.compute.internal>
2019-07-11 21:29:32 +0000

    wildlife stretch challenege complete

--- wildlife-tacker/config/routes.rb
+++ wildlife-tacker/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/sightings" => "sightings#create"
   put "/sightings/:id" => "sightings#update"
-  get "sightings/:date" => "sightings#index"
+  get "sightings" => "sightings#index"
 end
